4065abba9fc0778facd56c6d6a91ad88894d0f17
viniwii <vlad@saturdayteam.com>
2024-11-19 03:34:41 +0200

    Add division

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ def add(a, b):
 def substract(a, b):
     return a - b
 
+def divide(a, b):
+    if b != 0:
+        return a / b
+
 print('Welcome to the simple calculator!')
-print('Available operations: add, sub, mul')
+print('Available operations: add, sub, mul, div')
 
 operation = input('Enter the operation: ')
 num1 = float(input('a: '))
@@ -15,3 +19,5 @@ if operation == 'add':
     print(f'Result: {add(num1, num2)}')
 elif operation == 'sub':
     print(f'Result: {subtract(num1, num2)}')
+elif operation == 'div':
+    print(f'Result: {divide(num1, num2)}')
